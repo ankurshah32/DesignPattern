d80defca591dabc09351e0de5fa67697bd3bcb98
ankur shah <ankurshah32@gmail.com>
2019-08-16 16:26:12 +0530

    Add abstract class design pattern

---
+++ AbstractFactory.cs
@@ -0,0 +1,104 @@
+namespace AbstractFactory
+{
+    public interface IGUIFactory
+    {
+        IWinFactory GetWindowGui();
+        IMacFactory GetMacGui();
+        IMobileFactory GetMobileGui();
+    }
+    public interface IGUIFactory1 : IGUIFactory
+    {
+        IWebFactory GetWebGui();
+    }
+    public interface IAbstractFactory
+    {
+        void CreateBtn();
+        void CreateChkBox();
+    }
+    public interface IWinFactory: IAbstractFactory { }
+    public interface IMacFactory: IAbstractFactory { }
+    public interface IMobileFactory: IAbstractFactory { }
+    public interface IWebFactory : IAbstractFactory { }
+    public class ConcreteFactory : IGUIFactory
+    {
+        public IMacFactory GetMacGui()
+        {
+            return new Mac();
+        }
+
+        public IMobileFactory GetMobileGui()
+        {
+            return new Mobile();
+        }
+
+        public IWinFactory GetWindowGui()
+        {
+            return new Win();
+        }
+    }
+    public class ConcreteFactory1 : ConcreteFactory, IGUIFactory1
+    {
+        public IWebFactory GetWebGui()
+        {
+            return new Web(); ;
+        }
+    }
+
+    public class Web : IWebFactory
+    {
+        public void CreateBtn()
+        {
+            Console.WriteLine("Web Button");
+        }
+
+        public void CreateChkBox()
+        {
+            Console.WriteLine("Web Checkbox");
+        }
+    }
+    public class Win : IWinFactory
+    {
+        public void CreateBtn()
+        {
+            Console.WriteLine("Window Button");
+        }
+
+        public void CreateChkBox()
+        {
+            Console.WriteLine("Window Checkbox");
+        }
+    }
+    public class Mac : IMacFactory
+    {
+        public void CreateBtn()
+        {
+            Console.WriteLine("Mac Button");
+        }
+
+        public void CreateChkBox()
+        {
+            Console.WriteLine("Mac Checkbox");
+        }
+    }
+    public class Mobile : IMobileFactory
+    {
+        public void CreateBtn()
+        {
+            Console.WriteLine("Mobile Button");
+        }
+
+        public void CreateChkBox()
+        {
+            Console.WriteLine("Mobile Checkbox");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IGUIFactory1 fact = new ConcreteFactory1();
+            fact.GetMacGui().CreateBtn();
+            fact.GetWebGui().CreateBtn();
+        }
+    }
+}
